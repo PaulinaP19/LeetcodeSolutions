490ff4a8ad127c6b11dfac00e73c2ac3a06c4382
prorok <prorok@ROT7025.carlroth.local>
2024-12-28 12:59:35 +0100

    update

--- Leetcode/src/dailyChallenge/bestSightseeing1014.java
+++ Leetcode/src/dailyChallenge/bestSightseeing1014.java
@@ -1,5 +1,20 @@
 package dailyChallenge;
 
 public class bestSightseeing1014 {
+	public int maxScoreSightseeingPair(int[] values) {
+		int maxScore = 0;
+
+		for (int i = 0; i < values.length; i++) {
+			for (int j = i + 1; j < values.length && j < i + 1000; j++) {
+				int curSum = values[i] + values[j] + i - j;
+				if (curSum > maxScore) {
+					maxScore = curSum;
+				}
+
+			}
+
+		}
+		return maxScore;
+	}
 
 }
